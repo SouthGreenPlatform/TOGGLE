32c4f88045dbb208214d044d5f2a20661b5580d7
amanzoug <amansiham@gmail.com>
2015-04-03 10:27:06 +0200

    add of Module cufflinksRnaseq

--- Modules/cufflinks.pm
+++ Modules/cufflinks.pm
@@ -1,7 +1,5 @@
 package cufflinks;
 
-
-
 ###################################################################################################################################
 #
 # Copyright 2014 IRD-CIRAD
@@ -30,149 +28,54 @@ package cufflinks;
 #
 ###################################################################################################################################
 
-
-
 use strict;
 use warnings;
+
 use lib qw(.);
 use localConfig;
 use toolbox;
 use Data::Dumper;
 
-##CUFFLINKS
-##Module containing CUFFLINKS functions
-##
-###
-##Adding the tag XS 
-##XS tag is founded in bam file  coming from TopHat or Bowtie and not in other bam file such as Bwa
-sub addingXStag
+
+
+sub execution
 {
 
-    my ($file,$out) = @_;
-    
-    if ($file !~ m/\.bam$/)	##Check if the extension of the file in iput is ".bam"
-    {
-        warn ("\n$file is not a BAM file in adding XS tag\n");
-        return 0; 		###### 30-09 -CD : REVUE CODE pas gestion exportLog?
+    my ($cuffliksDirOut, $bamFileIn,$annotGffFile,$optionsHachees)=@_;
+
+    if ((toolbox::sizeFile($bamFileIn)==1) and (toolbox::sizeFile($annotGffFile)==1))             ##Check if the bamfile and annotGffFile exist and are not empty
+    {   
+
+
+        my $options=toolbox::extractOptions($optionsHachees, " ");  ##Get given options by software.configRnaSeq
+        ## DEBUGG toolbox::exportLog("DEBUG: cufflinks::execution : execution option equals to $options",1);
+
+        my $command=$cufflinks.$options." "." -o ".$cuffliksDirOut." -g ".$annotGffFile." ".$bamFileIn;				# command line to execute cutadapt
+ 
+        ##DEBUG
+        toolbox::exportLog("INFOS: cufflinks::execution : $command\n",1);
+
+        # Command is executed with the run function (package toolbox)
+        if (toolbox::run($command)==1)
+        {
+            toolbox::exportLog("INFOS: kufflinks : correctly done\n",1);
+            return 1;
+        }
+        else
+        {
+            toolbox::exportLog("ERROR: cufflinks : ABBORTED\n",0);
+            return 0;
+        }
+        
     }
     else
     {
-        ##Check if the bam file is correct
-	if (toolbox::checkSamOrBamFormat($file)==1)
-	{
-	    toolbox::exportLog("The bam file is a real binary file \n",1);
-            return 1;
-	}
-    
-        ##catch the name of the mapping tools in the bam file
-        my $test=system("samtools view -H $file | grep \@PG");
-        if ($test !~m/TopHat$/)
-	{
-            my $temp = "temp.sam";
-            my $xscom = "$samtools view -h $file | awk '{if(\$0 ~ /XS:A:/ || \$1 ~ /^@/) print \$0; else {if(and(\$2,0x10)) print \$0\"\tXS:A:-\"; else print \$0\"\tXS:A:+\";}}' > $temp ";
-            system("$xscom") and return 0; 			###### 30-09 -CD : REVUE CODE pas gestion par run?
-
-            my $bamcom = "$samtools view -bS -o $out $temp";
-            system("$bamcom") and return 0; 			###### 30-09 -CD : REVUE CODE pas gestion par run?
-            return 1;
-        }
-        
+        toolbox::exportLog("ERROR: cufflinks::execution : Problem with the files\n",0);
+        return 0;
     }
+    
     
 }
 
 
-
-##Assembly with Cufflinks
-sub cufflinks
-{
-    my($refFasta, $annotationGff, $alignementFile, $optionsHachees)=@_;
-    
-    #if (toolbox::sizeFile($refFastaFileIn)==1){     ##Check if the reference file exist and is not empty
-	my $options=toolbox::extractOptions($optionsHachees, " "); 
-        my $command= "$cufflinks/cufflinks"." $options"." -b $refFasta -g $annotationGff $alignementFile";
-        
-        toolbox::run($command);
-        if(toolbox::run($command)==1)
-        {
-            toolbox::exportLog("Assembly with Cufflinks done \n",1);
-            return 1;
-        }
-        else                                                                                                                                                                                                                    # if one or some previous files doesn't exist or is/are empty or if gatkBaseRecalibrator failed
-        {
-            toolbox::exportLog("Error when running cufflinks", 0);                                                                                                                                      # returns the error message
-        }
-
-}
-    
-
-
-##
-##
-##Merging with Cuffmerge
-sub cuffmerge
-{
-
-    my ($refFasta, $annotationGff, $inputDir, $inputFile, $outdir)=@_;
-
-    toolbox::makeDir($outdir);	    #create the outputdir
-    
-    system  ("ls $inputDir | grep '*.gtf' > list.txt");	#creation the assemblies.txt file ###### 30-09 -CD : REVUE CODE pas gestion par readDir?
-    
-    open (IN, "list.txt") or toolbox::exportLog("ERROR: cufflinks::cuffmerge : Cannot open the file list.txt $!\n",0); ###### 30-09 -CD : REVUE CODE fichier en dur?
-    open (OUT, ">assemblies.txt") or toolbox::exportLog("ERROR: cufflinks::cuffmerge : Cannot open the file assemblies.txt $!\n",0);   ###### 30-09 -CD : REVUE CODE fichier en dur?
-    while (<IN>)
-    {
-	if ($_ =~/^(.*)$/)
-	{
-	    my $file=$1;
-	    print OUT "./$file\n" ;
-	}
-    }
-    
-    my $cuffmergecommand =  $cufflinks."/cuffmerge". " -o $outdir  -s $refFasta -g $annotationGff  assemblies.txt";
-    #
-    #execution of the command
-    toolbox::run($cuffmergecommand);
-    
-    if(toolbox::run($cuffmergecommand)==1)
-    {
-        toolbox::exportLog("Mering with Cuffmerge done \n",1);
-        return 1;
-    }
-    else                                                                                                                                                                                                                    # if one or some previous files doesn't exist or is/are empty or if gatkBaseRecalibrator failed
-    {
-        toolbox::exportLog("Error when running Merging files", 0);                                                                                                                                      # returns the error message
-    }
-
-}
-
-
-##
-##
-##Cuffdiff
-#Index database sequences in the FASTA format.
-sub cuffdiff
-{
-    my($refFasta, $mergedFile, $bamFileIn, $annotation, $outdir, $optionsHachees)=@_;
-    
-    #if (toolbox::sizeFile($refFastaFileIn)==1){     ##Check if the reference file exist and is not empty
-        my $options=toolbox::extractOptions($optionsHachees, " "); 
-        # my $command=$bwa." index ".$options." ".$refFastaFileIn; ##command
-        my $command=$cufflinks."/cuffdiff". $options." -o $outdir -b $refFasta -u $mergedFile" ; 
-
-        toolbox::run($command);
-        if(toolbox::run($command)==1)
-        {
-            toolbox::exportLog("Assembly with Cufflinks done \n",1);
-            return 1;
-        }
-        else                                                                                                                                                                                                                    # if one or some previous files doesn't exist or is/are empty or if gatkBaseRecalibrator failed
-        {
-            toolbox::exportLog("Error when running cufflinks", 0);                                                                                                                                      # returns the error message
-            return 0;
-        }
-}
-
-    
 1;
