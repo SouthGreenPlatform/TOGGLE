c57872c831eb1ab45054745ee3aa3f041773250b
julieaorjuela <julieaorjuela@gmail.com>
2015-04-28 17:39:35 +0200

    changing sub checkNumberByWC by sub checkNumberByWC in Modules/fastqUtils.pm

--- Modules/fastqUtils.pm
+++ Modules/fastqUtils.pm
@@ -42,31 +42,45 @@ use Data::Dumper;
 
 
 #########################################
-#checkNumberByWC
-#check the sequences number using the wc -l command from bash
+#checkNumberLignes
+#check the sequences number using the wc -l command from bash and return the number of lines
 #########################################
-sub checkNumberByWC
-{ 
-    my ($fileName)=@_;		# recovery of informations
-    if (toolbox::checkFormatFastq($fileName)== 1)		# check if the file you give is a FASTQ file
-    {
-	toolbox::exportLog("INFOS: fastqUtils::checkNumberByWCl : The file $fileName is a fastq file\n",1);
-    }
-    else
-    {
-	toolbox::exportLog("ERROR: fastqUtils::checkNumberByWC : The file $fileName is not a fastq file\n",0);
-    }
-    my $nbLineCommand="wc -l ".$fileName; #command to count the line number
-    my $nbLine = `$nbLineCommand` or toolbox::exportLog("ERROR: fastqUtils::checkNumberByWC : Cannot run $nbLineCommand\n$!\n",0);	# execution of the command or if not possible, return an error message 
-    chomp $nbLine;
 
+sub checkNumberLignes
+{
+    my ($fileName)=@_;		# recovery of informations
+    my $nbLineCommand="wc -l ".$fileName; #command to count the line number
+    my $nbLine = `$nbLineCommand` or toolbox::exportLog("ERROR: fastqUtils::checkNumberLignes : Cannot run $nbLineCommand\n$!\n",0);	# execution of the command or if not possible, return an error message
+    chomp $nbLine;
+    
     #Add a split to only keep the number of line without the file name
     my @splitLine = split (" ", $nbLine);
     $nbLine = $splitLine[0];
-
-    my $numberOfReads = $nbLine/4;#Each sequence if made of 4 lines in Fastq
-    return $numberOfReads;
+    
+    return $nbLine;  # return the number of line of file
 }
+
+
+#########################################
+#checkNumberByWC
+#check the sequences number using the wc -l command from bash
+#########################################
+
+#sub checkNumberByWC
+#{
+#    my ($fileName)=@_;		# recovery of informations
+#    my $nbLineCommand="wc -l ".$fileName; #command to count the line number
+#    my $nbLine = `$nbLineCommand` or toolbox::exportLog("ERROR: fastqUtils::checkNumberByWC : Cannot run $nbLineCommand\n$!\n",0);	# execution of the command or if not possible, return an error message
+#    chomp $nbLine;
+
+    #Add a split to only keep the number of line without the file name
+#    my @splitLine = split (" ", $nbLine);
+#    $nbLine = $splitLine[0];
+
+#    my $numberOfReads = $nbLine/4;#Each sequence if made of 4 lines in Fastq
+#    return $numberOfReads;
+#}
+
 #########################################
 #checkEncodeByASCIIcontrol
 #Check the FASTQ format of a given file. 
