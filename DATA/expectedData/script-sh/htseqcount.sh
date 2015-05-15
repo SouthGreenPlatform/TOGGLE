a56295585e8e09630b4c67f3bd45791e0f370780
tranchant <christine.tranchant@ird.fr>
2015-05-15 10:27:48 +0200

    update software name

--- DATA/expectedData/script-sh/htseqcount.sh
+++ DATA/expectedData/script-sh/htseqcount.sh
@@ -1,2 +1,2 @@
 /usr/local/samtools-0.1.18/samtools-0.1.18/samtools sort -n ../tophat/accepted_hits.bam ../tophat/accepted_hits_SAMTOOLSSORT.bam
-/usr/local/bin/htseqcount -r=name -s=no -t=mRNA -m=union -i=ID -f=bam ../tophat/accepted_hits.SAMTOOLSSORT.bam ../referenceRNASeq.gff3 > ../tophat/accepted_hits.HTSEQCOUNT.txt
+/usr/local/bin/htseq-count -r=name -s=no -t=mRNA -m=union -i=ID -f=bam ../tophat/accepted_hits.SAMTOOLSSORT.bam ../referenceRNASeq.gff3 > ../tophat/accepted_hits.HTSEQCOUNT.txt
