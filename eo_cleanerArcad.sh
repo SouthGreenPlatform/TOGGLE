fcb92c1eb9875893ac501616f679d7107af1344a
tranchant <christine.tranchant@ird.fr>
2015-09-04 14:11:03 +0200

    remove cleaner

--- eo_cleanerArcad.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-rm DATA/referenceArcad.dict DATA/referenceArcad.fasta.amb DATA/referenceArcad.fasta.ann DATA/referenceArcad.fasta.bwt DATA/referenceArcad.fasta.fai DATA/referenceArcad.fasta.pac DATA/referenceArcad.fasta.sa
-
-rm -rf DATA/BamDirectory
-
-rm -rf DATA/arcadTest/*
-cp DATAbak/arcadTest/* DATA/arcadTest/
-
--- eo_cleanerIrigin.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-rm DATA/referenceIrigin.dict DATA/referenceIrigin.fasta.amb DATA/referenceIrigin.fasta.ann DATA/referenceIrigin.fasta.bwt DATA/referenceIrigin.fasta.fai DATA/referenceIrigin.fasta.pac DATA/referenceIrigin.fasta.sa
-
-rm -rf DATA/BamDirectory
-
-rm -rf DATA/iriginTest/*
-cp DATAbak/iriginTest/* DATA/iriginTest/
-
